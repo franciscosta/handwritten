7c52fdf1d424f599e104e3b08cd406775de5e016
Francisco Costa <franciscosta@hey.com>
2021-08-02 16:23:46 -0700

    Added alphabet and improved program

--- main.js
+++ main.js
@@ -4,17 +4,28 @@
 // . 46 , 44 ? 63 ! 33 - 45
 // + - * / & 43 45 42 47 = 61
 
+let WORD_COUNTER = 1;
 
+function initWord() {
 
+  WORD_COUNTER += 1;
 
-document.addEventListener('keypress', logKey);
+  let word = document.createElement('div');
+  word.classList = `word-${WORD_COUNTER}`;
+  document.querySelector('.letter').appendChild(word);
+}
 
-function logKey(e) {
-  let letter = document.querySelector('.letter');
+function deleteWord() {
+  const word = document.querySelector(`.letter`);
+  if (word.firstChild) word.removeChild(word.lastChild);
 
-  if (keyCheck(e.charCode)) {
-    letter.textContent += e.key;
+  WORD_COUNTER -= 1;
+
+  if (word.childElementCount === 0) {
+    WORD_COUNTER = 0;
+    initWord();
   }
+
 }
 
 function keyCheck(char) {
@@ -25,4 +36,50 @@ function keyCheck(char) {
   else if ([33, 44, 45, 46, 63, 43, 45, 42, 47, 61].includes(char)) return true;
 
   return false;
+}
+
+function appendCursor() {
+
+  // ????
+
+}
+
+function keyboard(e) {
+
+  const word = document.querySelector(`.word-${WORD_COUNTER}`);
+
+  // Keys 
+  if (keyCheck(e.charCode)) {
+
+    let img = document.createElement('img');
+    img.src = `letters/${e.charCode}.svg`;
+    word.appendChild(img);
+  } 
+  
+  // Space
+  if (e.code === 'Space') {
+    let img = document.createElement('img');
+    img.src = `letters/space.svg`;
+    word.appendChild(img); 
+
+    initWord();
+  }
+
+  // Delete
+  if (e.code === 'Backspace') {
+    deleteWord();
+  }
+
+}
+
+document.addEventListener('keypress', keyboard);
+
+
+// Save as image
+
+document.getElementById('button').onclick = function() {
+  domtoimage.toBlob(document.getElementById('save-this'))
+    .then(function(blob) {
+      window.saveAs(blob, 'handwritting.png');
+    });
 }
